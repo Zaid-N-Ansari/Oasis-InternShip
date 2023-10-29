531d68f01a9eabb9792c16132f2961932cee0070
Zaid Nasiruddin Ansari <125444268+Zaid-N-Ansari@users.noreply.github.com>
2023-10-29 17:21:39 +0530

    Update admin.py

--- ToDo/ToDoApp/admin.py
+++ ToDo/ToDoApp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import ToDoItem
-
-admin.site.register(ToDoItem)
+
 
 class AdminToDoItem(admin.ModelAdmin):
     list_display = ('ToDoItem_title', 'ToDoItem_desc')
+
+admin.site.register(ToDoItem, AdminToDoItem)
