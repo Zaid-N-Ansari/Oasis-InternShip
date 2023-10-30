00bb993a56a34e05518a6da4d81e039344c85937
Zaid Nasiruddin Ansari <125444268+Zaid-N-Ansari@users.noreply.github.com>
2023-10-30 21:21:37 +0530

    Add files via upload

---
+++ Calculator/static/js/app.js
@@ -0,0 +1,28 @@
+const inputBox = document.querySelector("input[type='text']");
+let buttons = Array.from(document.querySelectorAll("input[type='button']"));
+let hists = Array.from(document.querySelectorAll("td[name='calc_hist']"))
+
+let string = "";
+
+hists.forEach(hist => {
+    hist.addEventListener('click', function(e) {
+        inputBox.value = this.innerText;
+    });
+});
+
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if(e.target.value == 'AC'){
+            inputBox.value = string = "";
+        }
+        else if(e.target.value == 'DEL'){
+            string = inputBox.value;
+            string = string.substring(0, string.length-1);
+            inputBox.value = string;
+        }
+        else{
+            string += e.target.value;
+            inputBox.value = string;
+        }
+    });
+});
